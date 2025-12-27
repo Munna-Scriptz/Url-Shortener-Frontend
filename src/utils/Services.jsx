150d251f7a035774685e4967acfb9a51f0e6221a
Munna-Scripts <munnascriptz@gmail.com>
2025-12-27 19:50:04 +0600

    Time Ago Function created in utils

--- src/utils/Services.jsx
+++ src/utils/Services.jsx
@@ -8,4 +8,25 @@ export const getCookie = (name) => {
 };
 
 
+export const TimeAgo = (originalTime) => {
+    const timeAgo = (date) => {
+        const seconds = Math.floor((Date.now() - new Date(date)) / 1000);
 
+        if (seconds < 60) return `${seconds}s ago`;
+        const minutes = Math.floor(seconds / 60);
+        if (minutes < 60) return `${minutes}m ago`;
+        const hours = Math.floor(minutes / 60);
+        if (hours < 24) return `${hours}h ago`;
+        const days = Math.floor(hours / 24);
+        return `${days}d ago`;
+    };
+
+    const latestVisitTime =
+        originalTime.visitHistory.reduce(
+            (latest, curr) =>
+                new Date(curr.visitTime) > new Date(latest.visitTime) ? curr : latest
+        ).visitTime;
+        
+    const res = timeAgo(latestVisitTime)
+    return res
+}
